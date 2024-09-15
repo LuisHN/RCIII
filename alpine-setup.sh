84e4bddf8d6ab38d3cc05b70028e6a5f22f4f186
Lneves <luishenriqueneves1992@gmail.com>
2024-09-15 12:07:07 +0100

    Be Born!

---
+++ alpine-setup.sh
@@ -0,0 +1,20 @@
+# Exit if any command fails
+set -e
+
+# Update package list and upgrade existing packages
+echo "Updating package list and upgrading existing packages..."
+apk update
+apk upgrade
+
+# Install Git and Ansible
+echo "Installing Git and Ansible..."
+apk add git ansible
+
+# Clone playbook repository (modify this URL to your playbook repository)
+PLAYBOOK_REPO_URL="https://github.com/LuisHN/RCIII.git"
+PLAYBOOK_DIR="playbooks"
+
+echo "Cloning playbook repository from $PLAYBOOK_REPO_URL..."
+git clone "$PLAYBOOK_REPO_URL" "$PLAYBOOK_DIR"
+
+echo "Setup complete."
